1759b292827a875d34ae720db17a80c38179ceb3
WHLeshiy <aleksandr.panow2006@yandex.ru>
2025-05-22 11:28:45 +0300

    Удалил каталог build

--- build/Desktop_Qt_5_15_16_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_EchoServer_FILE C:/QT_projects/EchoServer/build/Desktop_Qt_5_15_16_shared_MinGW_w64_MINGW64_MSYS2-Debug/EchoServer.exe)
-set(__QT_DEPLOY_TARGET_EchoServer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_EchoServer_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Network.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
