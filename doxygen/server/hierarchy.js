9922e09a1d6e2ebe7e72e1567e67dbd0547dbbbe
Ekaterinakokonina200 <Ekaterinakokonina2006@gmail.com>
2025-06-06 23:24:57 +0300

    Add files via upload
    документация по кодам из ветки current_server папки include

---
+++ doxygen/server/hierarchy.js
@@ -0,0 +1,13 @@
+var hierarchy =
+[
+    [ "ClientState", "struct_client_state.html", null ],
+    [ "DatabaseManager", "class_database_manager.html", null ],
+    [ "RSA::KeyPair", "struct_r_s_a_1_1_key_pair.html", null ],
+    [ "PolynomialTask", "struct_polynomial_task.html", null ],
+    [ "QObject", null, [
+      [ "MyTcpServer", "class_my_tcp_server.html", null ]
+    ] ],
+    [ "RSA", "class_r_s_a.html", null ],
+    [ "TaskData", "struct_task_data.html", null ],
+    [ "TaskManager", "class_task_manager.html", null ]
+];
---
+++ doxygen/server/struct_r_s_a_1_1_key_pair.js
@@ -0,0 +1,5 @@
+var struct_r_s_a_1_1_key_pair =
+[
+    [ "privateKey", "struct_r_s_a_1_1_key_pair.html#aaa7130b4944bdc24dbff0ad7982b6d09", null ],
+    [ "publicKey", "struct_r_s_a_1_1_key_pair.html#a7d55f4a6cc91eaf5e657e2c9bb3dc4ca", null ]
+];
